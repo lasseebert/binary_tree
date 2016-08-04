65f07ef3eb91999167074c461a64fc65323f1e32
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2016-08-04 23:27:44 +0200

    Rename stuff to be more readable

--- lib/binary_tree.ex
+++ lib/binary_tree.ex
@@ -34,20 +34,20 @@ defmodule BinaryTree do
   end
 
   # Replace existing key with new value
-  def insert(%__MODULE__{key: key} = node, key, value) do
-    %{node | value: value}
+  def insert(%{key: key} = tree, key, value) do
+    %{tree | value: value}
   end
 
   # Insert in left subtree
-  def insert(%__MODULE__{key: root_key} = node, key, value) when key < root_key do
-    %{node | left: insert(node.left, key, value)}
+  def insert(%{key: root_key} = tree, key, value) when key < root_key do
+    %{tree | left: insert(tree.left, key, value)}
     |> skew
     |> split
   end
 
   # Insert in right subtree
-  def insert(%__MODULE__{key: root_key} = node, key, value) when key > root_key do
-    %{node | right: insert(node.right, key, value)}
+  def insert(%{key: root_key} = tree, key, value) when key > root_key do
+    %{tree | right: insert(tree.right, key, value)}
     |> skew
     |> split
   end
@@ -60,15 +60,15 @@ defmodule BinaryTree do
   def search(nil, _key), do: {:error, :not_found}
 
   # Search in tree with root key equal to search key
-  def search(%__MODULE__{key: key, value: value}, key), do: {:ok, value}
+  def search(%{key: key, value: value}, key), do: {:ok, value}
 
   # Search left subtree
-  def search(%__MODULE__{key: root_key, left: left}, key) when key < root_key do
+  def search(%{key: root_key, left: left}, key) when key < root_key do
     search(left, key)
   end
 
   # Search right subtree
-  def search(%__MODULE__{key: root_key, right: right}, key) when key > root_key do
+  def search(%{key: root_key, right: right}, key) when key > root_key do
     search(right, key)
   end
 
@@ -79,30 +79,30 @@ defmodule BinaryTree do
   def delete(nil, _key), do: nil
 
   # Delete root node with no children
-  def delete(%__MODULE__{key: key, left: nil, right: nil}, key), do: nil
+  def delete(%{key: key, left: nil, right: nil}, key), do: nil
 
   # Delete root node with left child
-  def delete(%__MODULE__{key: key, left: left, right: nil}, key), do: left
+  def delete(%{key: key, left: left, right: nil}, key), do: left
 
   # Delete root node with right child
-  def delete(%__MODULE__{key: key, left: nil, right: right}, key), do: right
+  def delete(%{key: key, left: nil, right: right}, key), do: right
 
   # Delete root node with two children. This is done by copying the in-order
   # predecessor to the root and then recursivley deleting the predecessor in the 
   # left subtree
-  def delete(%__MODULE__{key: key} = node, key) do
-    predecessor = max(node.left)
-    %{node | left: delete(node.left, predecessor.key), key: predecessor.key, value: predecessor.value}
+  def delete(%{key: key} = tree, key) do
+    predecessor = max(tree.left)
+    %{tree | left: delete(tree.left, predecessor.key), key: predecessor.key, value: predecessor.value}
   end
 
   # Delete in left subtree
-  def delete(%__MODULE__{key: root_key} = node, key) when key < root_key do
-    %{node | left: delete(node.left, key)}
+  def delete(%{key: root_key} = tree, key) when key < root_key do
+    %{tree | left: delete(tree.left, key)}
   end
 
   # Delete in right subtree
-  def delete(%__MODULE__{key: root_key} = node, key) when key > root_key do
-    %{node | right: delete(node.right, key)}
+  def delete(%{key: root_key} = tree, key) when key > root_key do
+    %{tree | right: delete(tree.right, key)}
   end
 
   @doc """
@@ -110,30 +110,30 @@ defmodule BinaryTree do
   Note: It traverses the entire tree. Use with caution
   """
   def height(nil), do: 0
-  def height(%__MODULE__{} = node) do
-    1 + Enum.max([height(node.left), height(node.right)])
+  def height(%{} = tree) do
+    1 + Enum.max([height(tree.left), height(tree.right)])
   end
 
   # Returns the maximum (right-most) subtree of a tree
-  defp max(%__MODULE__{right: nil} = node), do: node
-  defp max(%__MODULE__{right: right}), do: max(right)
+  defp max(%{right: nil} = tree), do: tree
+  defp max(%{right: right}), do: max(right)
 
   # Rotate tree to turn an invalid left horizontal link into a valid right
   # horizontal link
   defp skew(nil), do: nil
-  defp skew(%__MODULE__{left: nil} = node), do: node
-  defp skew(%__MODULE__{level: level, left: %__MODULE__{level: level}} = node) do
-    %{node.left | right: %{node | left: node.left.right}}
+  defp skew(%{left: nil} = tree), do: tree
+  defp skew(%{level: level, left: %{level: level}} = tree) do
+    %{tree.left | right: %{tree | left: tree.left.right}}
   end
-  defp skew(%__MODULE__{} = node), do: node
+  defp skew(%{} = tree), do: tree
 
   # Rotate tree to turn a double right horizontal link into a tree with a higher
   # level
   defp split(nil), do: nil
-  defp split(%__MODULE__{right: nil} = node), do: node
-  defp split(%__MODULE__{right: %__MODULE__{right: nil}} = node), do: node
-  defp split(%__MODULE__{level: level, right: %__MODULE__{right: %__MODULE__{level: level}}} = node) do
-    %{node.right | level: node.right.level + 1, left: %{node | right: node.right.left}}
+  defp split(%{right: nil} = tree), do: tree
+  defp split(%{right: %{right: nil}} = tree), do: tree
+  defp split(%{level: level, right: %{right: %{level: level}}} = tree) do
+    %{tree.right | level: tree.right.level + 1, left: %{tree | right: tree.right.left}}
   end
-  defp split(%__MODULE__{} = node), do: node
+  defp split(%{} = tree), do: tree
 end
